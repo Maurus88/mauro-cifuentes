22b640a3e8e00fd4c9dbc6bfda0bb9c0d5f188e0
Mauro Cifuentes <mauro.cifuentes88@gmail.com>
2022-12-02 17:14:41 -0300

    Update send.php

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+	echo "Error!";
 }
 $name = $_POST['nombre'];
 $visitor_email = $_POST['email'];
